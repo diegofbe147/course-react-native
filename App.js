1196914a839f2f2288702c05530837e86a0fac6a
diegofbe147 <diegofbe@gmail.com>
2020-02-06 21:29:26 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+import Home from "./src/screens/containers/home";
+
+export default class App extends Component {
+  render(){
+    return (
+      <Home>
+        <Text>Header</Text>
+        <Text>Buscador</Text>
+        <Text>Categorias</Text>
+        <Text>Sugerencias</Text>
+      </Home>
+    );
+  }
+};
