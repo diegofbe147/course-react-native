ffbfbfd51a1888989e475b76b7e9dc6ce86e7808
diegofbe147 <diegofbe@gmail.com>
2020-02-06 23:01:21 -0500

    Add header

---
+++ src/sections/components/header.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView
+} from 'react-native';
+
+function Header(props) {
+  return(
+    <SafeAreaView>
+      <View style={styles.mainContainer}>
+        <View style={styles.leftContainer}>
+          <Image 
+           source={require('../../../assets/logo.png')}
+           style={styles.logo}
+         />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.textRight}>
+            { props.children }
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    padding: 20,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    backgroundColor: 'purple',
+    flexDirection: 'row',
+    width: '50%',
+    justifyContent: 'flex-start'
+  },
+  rightContainer: {
+    backgroundColor: 'gray',
+    flexDirection: 'row',
+    width: '50%',
+    justifyContent: 'flex-end'
+  },
+  logo: {
+    width: 80,
+    height: 26,
+    resizeMode: 'contain',
+    backgroundColor: 'green',
+  },
+  textRight: {
+    backgroundColor: 'yellow',
+  }
+})
+
+export default Header;
